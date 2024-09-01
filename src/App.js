e6a7c126e34a44cc1a2d355fce6fc332aa567afe
bhoomigpt <bhoomigupta69@gmail.com>
2024-09-01 16:22:48 +0530

    uploading app

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Main from './Components/Main'
+import Signin from './Components/Signin'
+import { Route, Routes } from 'react-router-dom'
+import Chat from './Components/Chat'
+import PhoneSignin from './Components/PhoneSignin'
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<Signin/>}/>
+        <Route path='/main' element={<Main/>}/>
+        <Route path='/chat' element={<Chat/>}/>
+        <Route path='/phone' element={<PhoneSignin/>}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
